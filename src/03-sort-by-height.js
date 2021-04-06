df5f1a89695801928c2b0152208ceb0d3a921cc6
Gleb Stashinsky <tragy@list.ru>
2021-04-06 15:32:44 +0300

    feat: solve 03-sort-by-height task

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,26 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let temp;
+  let minIndex;
+  const sortedArr = [...arr];
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (sortedArr[i] !== -1) {
+      minIndex = i;
+      for (let j = i; j < sortedArr.length; j++) {
+        if (sortedArr[j] !== -1) {
+          if (sortedArr[minIndex] > sortedArr[j]) minIndex = j;
+        }
+      }
+      if (minIndex !== i) {
+        temp = sortedArr[minIndex];
+        sortedArr[minIndex] = sortedArr[i];
+        sortedArr[i] = temp;
+      }
+    }
+  }
+  return sortedArr;
 }
 
 module.exports = sortByHeight;

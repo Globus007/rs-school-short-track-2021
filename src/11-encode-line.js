16501455d58820eaec970fd10f6e6354465ab146
Gleb Stashinsky <tragy@list.ru>
2021-04-08 13:43:04 +0300

    feat: solve 11-encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let encoded = '';
+  let char = str[0] ? str[0] : '';
+  let count = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (char === str[i]) {
+      count++;
+    } else {
+      count = count > 1 ? count : '';
+      encoded += count + char;
+      char = str[i];
+      count = 1;
+    }
+  }
+  count = count > 1 ? count : '';
+  encoded += count + char;
+  return encoded;
 }
 
 module.exports = encodeLine;
